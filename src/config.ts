f86004621ffaac8cb80acdd732e839dcc5c101d0
unknown <3525140179@qq.com>
2025-08-25 19:10:27 +0800

    feat : 添加旋转头像

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,9 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "Qiuarctica",
+	subtitle: "Personal blog",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -58,7 +58,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/meihong.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Qiuarctica",
 	bio: "莲瓣入水而不苦根茎，勿执着",
 	links: [
